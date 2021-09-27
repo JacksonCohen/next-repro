4ecf3966aa84afd4d3f2bfa8832d916be8d7033f
Jackson Cohen <jacksonrcohen@gmail.com>
2021-09-27 18:17:52 -0400

    Add repro changes

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: '/account/:slug(\\onboarding|login|registration)',
+        destination: '/dashboard',
+        permanent: false,
+      },
+    ];
+  },
+};
